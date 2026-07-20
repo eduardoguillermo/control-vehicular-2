626a3847912fafb47956b4272d628010432f1f7c
eduardoguillermo <70851744+eduardoguillermo@users.noreply.github.com>
2026-07-20 17:51:52 -0300

    Initial commit: sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'control-vehicular-v0.01';
+const ASSETS = [
+  '/control-vehicular/',
+  '/control-vehicular/index.html',
+  '/control-vehicular/app.js',
+  '/control-vehicular/drive-sync.js',
+  '/control-vehicular/style.css',
+  '/control-vehicular/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if(res && res.status === 200 && e.request.method === 'GET'){
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});
